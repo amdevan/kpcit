24e8ae5fdbbb9b800f535f1a57eb383de3ab9f49
Devan <devanthakur@Devans-MacBook-Air.local>
2026-06-08 12:13:39 +0545

    Run api/edge as Node on Vercel

--- api/edge.ts
+++ api/edge.ts
@@ -1,7 +1,7 @@
 import app from "../src/server";
 
 export const config = {
-  runtime: "edge",
+  runtime: "nodejs",
 };
 
 export default async function handler(request: Request): Promise<Response> {
@@ -16,4 +16,3 @@ export default async function handler(request: Request): Promise<Response> {
   // Vercel Edge doesn't provide Cloudflare-style env/ctx; pass empty objects.
   return app.fetch(new Request(url.toString(), request), {}, {});
 }
-
